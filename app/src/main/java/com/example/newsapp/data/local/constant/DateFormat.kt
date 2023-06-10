6bec92a243e94595adfe83352b3154881971d63e
shinya-okochi <shny.kch@gmail.com>
2023-06-11 02:46:43 +0900

    DateFormatが異なって例外が発生していた問題の修正

--- app/src/main/java/com/example/newsapp/data/local/constant/DateFormat.kt
+++ app/src/main/java/com/example/newsapp/data/local/constant/DateFormat.kt
@@ -5,6 +5,6 @@ import java.util.Locale
 
 object DateFormat {
     private val local: Locale = Locale.JAPAN
-    val ISO_8601 = SimpleDateFormat("yyyy-MM-ddTHH:mm:ssZ", local)
+    val ISO_8601 = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX", local)
     val JAPANESE_DATE_TIME = SimpleDateFormat("yyyy年M月d日(E) HH時mm分", local)
 }
