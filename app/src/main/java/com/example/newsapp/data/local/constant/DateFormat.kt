b8a58541bb3df8b6e439acf6048a1311d68e3b13
shinya-okochi <shny.kch@gmail.com>
2023-06-11 00:50:38 +0900

    News：セルの表示処理実装

---
+++ app/src/main/java/com/example/newsapp/data/local/constant/DateFormat.kt
@@ -0,0 +1,10 @@
+package com.example.newsapp.data.local.constant
+
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+object DateFormat {
+    private val local: Locale = Locale.JAPAN
+    val ISO_8601 = SimpleDateFormat("yyyy-MM-ddTHH:mm:ssZ", local)
+    val JAPANESE_DATE_TIME = SimpleDateFormat("yyyy年M月d日(E) HH時mm分", local)
+}
