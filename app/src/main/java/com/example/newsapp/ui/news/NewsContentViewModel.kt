3a9df1fb9c9009e29dbcb33802b5a1210b96fa96
shinya-okochi <shny.kch@gmail.com>
2023-06-11 00:28:22 +0900

    News：Category選択時のUI実装

--- app/src/main/java/com/example/newsapp/ui/news/NewsContentViewModel.kt
+++ app/src/main/java/com/example/newsapp/ui/news/NewsContentViewModel.kt
@@ -2,6 +2,14 @@ package com.example.newsapp.ui.news
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.example.newsapp.data.remote.newsApi.response.common.Article
 
 class NewsContentViewModel(app: Application) : AndroidViewModel(app) {
+    // View表示用変数
+    var isLoading = MutableLiveData(false)
+    var isError = MutableLiveData(false)
+
+    var newsContentAdapter : NewsContentAdapter? = null
+    var newsList = mutableListOf<Article>()
 }
